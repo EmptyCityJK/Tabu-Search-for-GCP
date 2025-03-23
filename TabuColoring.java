3900ac2fb0e9dc72f2d5054428c0a93bb168efdf
llz <2460752764@qq.com>
2025-03-23 15:48:56 +0800

    init project

---
+++ TabuColoring.java
@@ -0,0 +1,11 @@
+package ynu.ls.coloring; 
+
+public class TabuColoring {
+      public static void main(String[] args) {
+        
+      }    
+      public static int[] search() {
+
+      }
+}
+
